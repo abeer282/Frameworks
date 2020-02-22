b668e75537f20d0295a27b006e98b5c69c6399d1
abeer282 <abeerala282@gmail.com>
2020-02-22 14:28:07 +0200

    Add setter injection with map and list

--- SpringFramework/SetterDI/src/main/java/com/abeer/Person.java
+++ SpringFramework/SetterDI/src/main/java/com/abeer/Person.java
@@ -1,10 +1,18 @@
 package com.abeer;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Person {
 	private int id;
 	private String personName;
 	private String favColor;
 	private Address addr;
+	private List<String> nickNames;
+	private Map<String,String> singers;
 
 	public int getId() {
 		return id;
@@ -40,9 +48,35 @@ public class Person {
 	public void setAddr(Address addr) {
 		this.addr = addr;
 	}
+	
+	public List<String> getNickNames() {
+		return nickNames;
+	}
+
+	public void setNickNames(List<String> nickNames) {
+		this.nickNames = nickNames;
+	}
+
+	public Map<String, String> getSingers() {
+		return singers;
+	}
+
+	public void setSingers(Map<String, String> singers) {
+		this.singers = singers;
+	}
 
 	void display() {
 		System.out.println("ID: "+id+", Name: "+personName+", Favorite color: "+ favColor+"\nAddress: "+addr.toString());
+		Iterator<String> it = nickNames.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		Set<Entry<String,String>> set=singers.entrySet();
+		Iterator<Entry<String,String>> it1 = set.iterator();
+		while(it1.hasNext()) {
+			Entry<String,String> entry=it1.next();
+			System.out.println("Key: "+entry.getKey()+", Value: "+entry.getValue());
+		}
 	}
 
 }
