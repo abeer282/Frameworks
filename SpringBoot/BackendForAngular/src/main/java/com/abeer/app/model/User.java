a4317ad2e13a22923ffbc34d5902b00f308251dc
abeer282 <abeerala282@gmail.com>
2020-03-23 19:58:16 +0200

    Add model

---
+++ SpringBoot/BackendForAngular/src/main/java/com/abeer/app/model/User.java
@@ -0,0 +1,34 @@
+package com.abeer.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	@Id
+	@Column(name="id")
+	@GeneratedValue()
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
