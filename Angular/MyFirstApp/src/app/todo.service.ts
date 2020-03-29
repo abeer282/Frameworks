6a21a00996e84bc4df67ed408c2b29808e42059c
abeer282 <abeerala282@gmail.com>
2020-03-29 20:23:12 +0300

    Add service

---
+++ Angular/MyFirstApp/src/app/todo.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { loadavg } from 'os';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+todoList=['aaaa','bbbb','cccc','ddddd'];
+  constructor(private http:HttpClient) { this.load()}
+  load(){
+    
+  }
+  addItem(task:string, token:string){
+this.todoList.unshift(task);
+  }
+}
