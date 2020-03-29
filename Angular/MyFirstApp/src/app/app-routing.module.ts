a91c5544dd63ab30d1d851e0d293c3283c8599f3
abeer282 <abeerala282@gmail.com>
2020-03-29 15:17:44 +0300

    Add form contact

--- Angular/MyFirstApp/src/app/app-routing.module.ts
+++ Angular/MyFirstApp/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
-import { AppComponent } from './app.component';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
-              {path: "",component:AppComponent}
+  {path: "", redirectTo:"home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "*",component:NotfoundComponent}
 ];
 
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes) ],
+  imports: [RouterModule.forRoot(routes)
+   ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Angular/MyFirstApp/src/app/app.module.ts
+++ Angular/MyFirstApp/src/app/app.module.ts
@@ -2,17 +2,27 @@ import { TokenService } from './token.service';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import {  HttpClientModule } from '@angular/common/http';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    NotfoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule 
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [TokenService],
   bootstrap: [AppComponent]
