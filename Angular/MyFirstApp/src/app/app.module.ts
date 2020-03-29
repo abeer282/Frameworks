82f5a4969cf503b8d7a2a606c03f609eee78d27b
abeer282 <abeerala282@gmail.com>
2020-03-29 15:26:44 +0300

    Remove unused component

--- Angular/MyFirstApp/src/app/app.module.ts
+++ Angular/MyFirstApp/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import {  HttpClientModule } from '@angular/common/http';
-import { ContactComponent } from './contact/contact.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
+import { AddtaskComponent } from './addtask/addtask.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent,
-    NotfoundComponent,
-    HomeComponent
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    TasklistComponent,
+    AddtaskComponent
   ],
   imports: [
     BrowserModule,
