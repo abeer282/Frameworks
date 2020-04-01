e70f3a20e89197ada06bbf26413912360d71baaf
abeer282 <abeerala282@gmail.com>
2020-04-02 01:15:58 +0300

    Add login function

--- Angular/MyFirstApp/src/app/login/login.component.ts
+++ Angular/MyFirstApp/src/app/login/login.component.ts
@@ -1,15 +1,33 @@
+import {  Router } from '@angular/router';
+import { TokenService } from './../token.service';
 import { Component, OnInit } from '@angular/core';
+import { UserData } from '../UserData';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  constructor(private tokenService:TokenService, private router:Router) { }
 
-  ngOnInit(): void {
+  userData: UserData = new UserData();
+  error=false;
+
+ 
+  login(){
+    if(this.userData.username.length > 0 && this.userData.password.length > 0){
+      this.tokenService.getToken(this.userData)
+      .subscribe(res=> {
+
+            res.expires_in=new Date().getTime()+res.expires_in*1000;
+
+            sessionStorage.setItem('jsessionid',JSON.stringify(res));
+            
+            this.router.navigateByUrl('/home');
+          }, 
+      error=> this.error=true);
+    }
   }
-
 }
