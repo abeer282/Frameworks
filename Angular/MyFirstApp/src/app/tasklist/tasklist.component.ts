80d80d142863312696c1f2054d410dafb8c527dc
abeer282 <abeerala282@gmail.com>
2020-04-02 01:20:55 +0300

    Add task list logic

--- Angular/MyFirstApp/src/app/tasklist/tasklist.component.ts
+++ Angular/MyFirstApp/src/app/tasklist/tasklist.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasklistComponent implements OnInit {
 
-  constructor(private todo:TodoService) { }
+  constructor(public todo:TodoService) { }
 
   ngOnInit(): void {
   }
