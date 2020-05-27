70bba3882982c50b3faf8d4981681606c4d46cb2
abeer282 <abeerala282@gmail.com>
2020-05-27 17:00:23 +0300

    Add controller

---
+++ ASP.NET/DrugApi/Controllers/DrugsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DrugApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DrugApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrugsController : Controller
+    {
+        private readonly DrugContext _context;
+
+        public DrugsController(DrugContext context)
+        {
+            _context = context;
+            if(_context.Drugs.Count () == 0)
+            {
+                _context.Drugs.Add(new Drug { Name = "Drug1" });
+                _context.SaveChanges();
+            }
+        }
+
+    }
+
+
+}
