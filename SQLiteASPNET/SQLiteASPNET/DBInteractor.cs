544415bb9a0aaf755a08fd11a96f9ee6e55076dc
abeer282 <abeerala282@gmail.com>
2020-05-31 22:58:08 +0300

    Add asp dot net and sqlite project

---
+++ SQLiteASPNET/SQLiteASPNET/DBInteractor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SQLiteASPNET;
+
+namespace SQLiteASPNET
+{
+    public class DBInteractor : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder
+            .UseSqlite(@"Data Source = Students.db;");
+        }
+        public DbSet<StudentsInfo> StudentsInfo { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentsInfo>().HasData(
+            new StudentsInfo() { Id = 1, stuPrefix = "Ms.", stuFirstName = "Amelia", stuLastName = "Petter", stuGrade = 6, stuAge = 11, stuDayScholar = "Yes", stuOptionalLang = "French" },
+            new StudentsInfo() { Id = 2, stuPrefix = "Mr.", stuFirstName = "Richard", stuLastName = "Grey", stuGrade = 7, stuAge = 12, stuDayScholar = "No", stuOptionalLang = "Spanish" }
+            );
+
+        }
+    }
+    
+}
---
+++ SQLiteASPNET/SQLiteASPNET/StudentsInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLiteASPNET
+{
+    public class StudentsInfo
+    {
+        public long Id { get; set; }
+        public string stuPrefix { get; set; }
+        public string stuFirstName { get; set; }
+        public string stuLastName { get; set; }
+        public int stuGrade { get; set; }
+        public int stuAge { get; set; }
+        public string stuDayScholar { get; set; }
+        public string stuOptionalLang { get; set; }
+    }
+}
